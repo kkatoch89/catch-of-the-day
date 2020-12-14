0d2d0788b494ede408cb793f0d918aafaa3b9c1d
kkatoch89 <karankatoch.hba@gmail.com>
2020-12-13 21:08:25 -0500

    Added animation in order section; animations for individual orders that are added/removed/qty changed

--- src/components/Order.js
+++ src/components/Order.js
@@ -13,14 +13,16 @@ class Order extends Component {
 
 		const isAvailable = fish.status === 'available';
 
+		const transitionOptions = {
+			classNames: 'order',
+			key,
+			timeout: { enter: 500, exit: 500 },
+		};
+
 		if (!isAvailable) {
-			console.log(isAvailable);
+			// console.log(isAvailable);
 			return (
-				<CSSTransition
-					classNames="order"
-					key={key}
-					timeout={{ enter: 250, exit: 250 }}
-				>
+				<CSSTransition {...transitionOptions}>
 					<li key={key}>
 						Sorry {fish ? fish.name : 'fish'} is no longer available
 					</li>
@@ -28,21 +30,28 @@ class Order extends Component {
 			);
 		}
 		return (
-			<CSSTransition
-				classNames="order"
-				key={key}
-				timeout={{ enter: 250, exit: 250 }}
-			>
+			<CSSTransition {...transitionOptions}>
 				<li key={key}>
-					{count} lbs {fish.name}
-					{formatPrice(count * fish.price)}
-					<button
-						onClick={() => {
-							this.props.removeFromOrder(key);
-						}}
-					>
-						&times;
-					</button>
+					<span>
+						<TransitionGroup component="span" className="count">
+							<CSSTransition
+								classNames="count"
+								key={count}
+								timeout={{ enter: 500, exit: 500 }}
+							>
+								<span>{count}</span>
+							</CSSTransition>
+						</TransitionGroup>
+						lbs {fish.name}
+						{formatPrice(count * fish.price)}
+						<button
+							onClick={() => {
+								this.props.removeFromOrder(key);
+							}}
+						>
+							&times;
+						</button>
+					</span>
 				</li>
 			</CSSTransition>
 		);
